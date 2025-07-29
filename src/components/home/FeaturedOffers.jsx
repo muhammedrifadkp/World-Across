166fb1cdad5b0c39a4d70469aeb243e5fd0ce0ce
muhammedrifadkp <muhammedrifadkp3@gmail.com>
2025-07-29 11:57:59 +0530

    cleanup: Remove placeholder-image

--- src/components/home/FeaturedOffers.jsx
+++ src/components/home/FeaturedOffers.jsx
@@ -172,8 +172,6 @@ const FeaturedOffers = () => {
                   blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
                   onError={(e) => {
                     console.log('Image failed to load:', offer.image);
-                    // Hide image on error since we have reliable local images
-                    e.target.style.display = 'none';
                   }}
                 />
                 
